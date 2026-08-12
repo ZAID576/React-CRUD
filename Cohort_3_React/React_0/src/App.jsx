e0e6c8b4ded156380513ff5d74108e087ee1c825
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-12 15:26:56 +0530

    Creating & merging different components in App.jsx

---
+++ Cohort_3_React/React_0/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Contact from './Component/Contact'
+import Hero from './Component/Hero'
+import Footer from './Component/Footer'
+
+const App = () => {
+  return (
+    <div>
+      {/* <h1>Hey i am app</h1> */}
+      <Hero />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
