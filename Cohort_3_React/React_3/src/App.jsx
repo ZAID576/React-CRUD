566d38c291cd7577e5add12a127a86d31f6c5d57
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-16 11:53:57 +0530

    Brute force approach of Form Handling

--- Cohort_3_React/React_3/src/App.jsx
+++ Cohort_3_React/React_3/src/App.jsx
@@ -2,16 +2,27 @@ import React, { use, useState } from 'react'
 
 const App = () => {
 const [name, setName] = useState("")
+const [email, setEmail] = useState("")
+const [password, setPassword] = useState("")
+
 console.log(name)
+console.log(email)
+console.log(password)
   
   return (
     <div className='flex flex-col gap-5 w-60'>
       <input onChange={(e)=> setName(e.target.value)} className='border-2' type="text" placeholder='Name'/>
-      <input className='border-2' type="text" placeholder='Email'/>
-      <input className='border-2' type="text" placeholder='Password'/>
+
+      <input onChange={(e)=> setEmail(e.target.value)} className='border-2' type="text" placeholder='Email'/>
+      
+      <input onChange={(e)=> setPassword(e.target.value)} className='border-2' type="text" placeholder='Password'/>
 
       <button className='border-2'>Submit</button>
+
        <h1>this is -{name}</h1>
+       <h1>this is -{email}</h1>
+       <h1>this is -{password}</h1>
+
     </div>
   )
 }
