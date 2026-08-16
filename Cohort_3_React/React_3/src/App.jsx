2fbcc1298131f845b7fad331477b4917e8cbf89d
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-16 11:04:59 +0530

    applying useState to the onChange so that react know the changes happening in yhe form input and renders the whole UI

--- Cohort_3_React/React_3/src/App.jsx
+++ Cohort_3_React/React_3/src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 
 const App = () => {
+const [name, setName] = useState("")
+console.log(name)
+  
   return (
     <div className='flex flex-col gap-5 w-60'>
-      <input onChange={(e)=>{console.log(e.target.value)}} className='border-2' type="text" placeholder='Name'/>
+      <input onChange={(e)=> setName(e.target.value)} className='border-2' type="text" placeholder='Name'/>
       <input className='border-2' type="text" placeholder='Email'/>
       <input className='border-2' type="text" placeholder='Password'/>
 
       <button className='border-2'>Submit</button>
+       <h1>this is -{name}</h1>
     </div>
   )
 }
