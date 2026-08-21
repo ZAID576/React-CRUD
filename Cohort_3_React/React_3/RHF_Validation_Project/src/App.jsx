aebc2b2450e9eac29a1771bc08463f584525e72c
Mohammad Zaid Khan <zkonline576@gmail.com>
2026-08-21 08:52:52 +0530

    App.jsx

--- Cohort_3_React/React_3/RHF_Validation_Project/src/App.jsx
+++ Cohort_3_React/React_3/RHF_Validation_Project/src/App.jsx
@@ -4,19 +4,28 @@ import Usercard from "./Components/Usercard";
 import Form from "./Components/Form";
 
 const App = () => {
-   const [toggle, setToggle] = useState(true)
+   const [toggle, setToggle] = useState(true);  
+   const [user, setUser] = useState([]);
+
+  //  const deleteProduct = (id) => {
+  //    const product = user.filter((elem) => elem.id !== id);
+  //    setUser(product);
+  //  }
 
   return (
     <div className="p-3 h-screen flex flex-col gap-4" >
       <Navbar setToggle={setToggle}/>
 
       {toggle ? (
-        <div className="flex">
-        <Usercard />
+        <div className="flex gap-10  ">
+       {
+       user.map((elem)=>
+        <Usercard user={elem}/>)
+       }  
       </div>
       ) : (
         <div className="flex justify-center h-[70%] items-center">
-          <Form />
+          <Form setToggle={setToggle} setUser={setUser}/>
         </div>
       ) 
       }
